1db6b77ae4cca4e6ac046e47ef1b7dd939be109f
9KelvinN <9kelvin@gmail.com>
2020-12-08 01:38:04 -0800

    Got Host Room Code working
    Added socket script to index.html, made sockets for hostGame

--- server.js
+++ server.js
@@ -12,12 +12,19 @@ app.use(express.static("public"));
 
 let io = socketIO(server);
 
+let games = [];
 io.on('connection', (socket) => {
-    console.log('A user just connected.');
-    socket.on('disconnect', () => {
-        console.log('A user has disconnected.');
-    })
+
+    socket.on('hostGame', () => {
+      //Generate random number from 0000 - 9999
+      var joinCode = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+      console.log(joinCode);
+      // To implement feature ensuring different numbers 
+      io.emit('hostGame', joinCode);
+    });
 });
+
+
 
 server.listen(port, () => {
   console.log('listening on *:' + port);
